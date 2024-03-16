94a159ec4f43db665aadbc663c689c6358b8f1ab
treboB <robertren123.abc@gmail.com>
2024-03-16 15:02:49 -0700

    fixed indexing on simple moving average

--- MovingAverages.py
+++ MovingAverages.py
@@ -3,8 +3,7 @@ import pandas as pd
 import yfinance as yf
 import datetime
 import statistics as stat
-
-#calculate n day moving avreage
+    
 def calculate_moving_average(stock_symbol, n_days, start_date, end_date):
     # Fetch historical market data
     data = yf.Ticker(stock_symbol)
@@ -18,13 +17,11 @@ def calculate_moving_average(stock_symbol, n_days, start_date, end_date):
     if(starting_index < 0):
         return None
     ending_index = full_data.index.get_loc(ending_row_name)
-    dataList = full_data['Close'].tolist()[starting_index:ending_index]
-    
+    dataList = full_data['Close'].tolist()[starting_index:ending_index + 1]
     #find moving average
     upper_index =  n_days - 1
     lower_index = 0
     moving_average = []
-    print((dataList[upper_index]))
     while(upper_index < len(dataList)):
         moving_average.append(stat.mean(dataList[lower_index:upper_index]))
         upper_index += 1
@@ -33,8 +30,37 @@ def calculate_moving_average(stock_symbol, n_days, start_date, end_date):
     
 
 #calculate a n-day exponential moving average
-def nDayEMA(days, start, end):
-    pass
+def nDayEMA(stock_symbol, n_days, start_date, end_date):
+     # Fetch historical market data
+    data = yf.Ticker(stock_symbol)
+    historical_data = data.history(start=start_date, end=end_date)
+    starting_row_name = historical_data.iloc[0].name
+    ending_row_name = historical_data.iloc[len(historical_data) - 1].name
+
+    # Fetch relevant data to find moving average
+    full_data = data.history(period="max")
+    starting_index = full_data.index.get_loc(starting_row_name) - 1
+    ending_index = full_data.index.get_loc(ending_row_name)
+
+    dataList = full_data['Close'].tolist()[starting_index:ending_index]
+    k = 2.0 / (n_days + 1)
+    yesterdays_ema = dataList[0]
+    print(yesterdays_ema)
+    
+    ema = []
+    first = True
+    for price in dataList:
+        if(first):
+            first = False
+            break
+        print(price)
+        new_ema = (price - yesterdays_ema) * k + yesterdays_ema
+        ema.append(new_ema)
+        yesterdays_ema = new_ema
+    return ema
+        
+        
+    
 
 
 #calculate a golden cross between a s-day moving average, and a l-day moving average
@@ -48,6 +74,8 @@ def goldenCross(short, long):
 stock_symbol = 'AAPL'  # Apple Inc.
 n_days = 50
 start_date = datetime.datetime(2024, 3, 5)
-end_date = datetime.datetime(2024, 3, 13)
+end_date = datetime.datetime(2024, 3, 6)
 
 print(calculate_moving_average(stock_symbol, n_days, start_date, end_date))
+thing = [1, 2, 3, 4]
+print(thing[0:len(thing)])
