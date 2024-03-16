cb0263d6c9331b7ffb83ff655d3f3621e3017ccb
treboB <robertren123.abc@gmail.com>
2024-03-16 12:46:36 -0700

    Simple moving averages seem good, going to work on EMA and crosssing next

--- MovingAverages.py
+++ MovingAverages.py
@@ -2,6 +2,7 @@ import math
 import pandas as pd
 import yfinance as yf
 import datetime
+import statistics as stat
 
 #calculate n day moving avreage
 def calculate_moving_average(stock_symbol, n_days, start_date, end_date):
@@ -11,19 +12,24 @@ def calculate_moving_average(stock_symbol, n_days, start_date, end_date):
     starting_row_name = historical_data.iloc[0].name
     ending_row_name = historical_data.iloc[len(historical_data) - 1].name
 
+    # Fetch relevant data to find moving average
     full_data = data.history(period="max")
-    starting_index = full_data.index.get_loc(starting_row_name) - n_days
+    starting_index = full_data.index.get_loc(starting_row_name) - (n_days - 1) #+1 because the first days closing price is included in the calculation
     if(starting_index < 0):
         return None
     ending_index = full_data.index.get_loc(ending_row_name)
     dataList = full_data['Close'].tolist()[starting_index:ending_index]
-    return dataList
-
-
     
-    #get the first value of the historical data dataframe
-    #then get the max data frame and find that value in the dataframe, go back n rows, and then we start
-    
+    #find moving average
+    upper_index =  n_days - 1
+    lower_index = 0
+    moving_average = []
+    print((dataList[upper_index]))
+    while(upper_index < len(dataList)):
+        moving_average.append(stat.mean(dataList[lower_index:upper_index]))
+        upper_index += 1
+        lower_index += 1
+    return moving_average
     
 
 #calculate a n-day exponential moving average
