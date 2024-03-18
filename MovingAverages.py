4b98b798823f81892ef039c1fc7c5dfb578d5560
treboB <robertren123.abc@gmail.com>
2024-03-17 22:55:41 -0700

    have code that can calculate the crossing, divergence, and convergence of two moving averages

--- MovingAverages.py
+++ MovingAverages.py
@@ -4,7 +4,7 @@ import yfinance as yf
 import datetime
 import statistics as stat
     
-def calculate_moving_average(stock_symbol, n_days, start_date, end_date):
+def calculate_moving_average(stock_symbol, n_days, start_date, end_date) -> pd.DataFrame:
     data = yf.Ticker(stock_symbol) #get the stock data
     historical_data = data.history(start=start_date, end=end_date) #get the data from start date to end date, to get the indexes
     
@@ -58,13 +58,37 @@ def nDayEMA(stock_symbol, n_days, start_date, end_date):
         yesterdays_ema = new_ema
     return ema
         
-        
-    
+#calculate crosses, and divergence/convergence between two moving averages
+def sgn(n):
+    if(n > 0):
+        return 1
+    if(n < 0):
+        return -1
+    return 0
 
+def movingAverageCross(moving_average1: pd.DataFrame, moving_average2: pd.DataFrame) -> pd.DataFrame:
+    aligned_data = pd.merge(moving_average1, moving_average2, left_index=True, right_index=True, how='inner', suffixes=('_1', '_2'))
+    aligned_data['difference'] = aligned_data['Close_1'] - aligned_data['Close_2']
+    prev_difference = 0
+    convergence_list = []
+    divergence_list = []
+    cross_list = []
+    for i in range(len(aligned_data)):
+        if(i == 0):
+            convergence_list.append(False)
+            divergence_list.append(False)
+            cross_list.append(False)
+            prev_difference = aligned_data['difference'][i]
+        else:
+            convergence_list.append(math.fabs(aligned_data['difference'][i]) <= math.fabs(prev_difference))
+            divergence_list.append(math.fabs(aligned_data['difference'][i]) >= math.fabs(prev_difference))
+            cross_list.append(sgn(prev_difference) == -1 * sgn(aligned_data['difference'][i]))
+            prev_difference = aligned_data['difference'][i]
+    aligned_data['Convergence'] = convergence_list
+    aligned_data['Divergence'] = divergence_list
+    aligned_data['Cross'] = cross_list
+    return aligned_data
 
-#calculate a golden cross between a s-day moving average, and a l-day moving average
-def goldenCross(short, long):
-    pass
-
-moving_average = calculate_moving_average('AAPL', 20, '2023-01-01', '2023-03-01')
-print(moving_average)
+moving_average = calculate_moving_average('AAPL', 20, '2023-01-06', '2023-03-04')
+moving_average2 = calculate_moving_average('AAPL', 50, '2023-01-01', '2023-03-04')
+print(movingAverageCross(moving_average, moving_average2))
