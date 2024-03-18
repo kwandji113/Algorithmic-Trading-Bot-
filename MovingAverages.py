288bc03f5ea05ddd2a3c6295732ca87ab918e0b9
treboB <robertren123.abc@gmail.com>
2024-03-17 22:13:32 -0700

    Made SMA code cleaner, made it so that it also gives you the date of each moving average in a dataframe

--- MovingAverages.py
+++ MovingAverages.py
@@ -5,29 +5,27 @@ import datetime
 import statistics as stat
     
 def calculate_moving_average(stock_symbol, n_days, start_date, end_date):
-    # Fetch historical market data
-    data = yf.Ticker(stock_symbol)
-    historical_data = data.history(start=start_date, end=end_date)
-    starting_row_name = historical_data.iloc[0].name
-    ending_row_name = historical_data.iloc[len(historical_data) - 1].name
-
-    # Fetch relevant data to find moving average
-    full_data = data.history(period="max")
-    starting_index = full_data.index.get_loc(starting_row_name) - (n_days - 1) #+1 because the first days closing price is included in the calculation
-    if(starting_index < 0):
-        return None
-    ending_index = full_data.index.get_loc(ending_row_name)
-    dataList = full_data['Close'].tolist()[starting_index:ending_index + 1]
-    #find moving average
-    upper_index =  n_days - 1
-    lower_index = 0
-    moving_average = []
-    while(upper_index < len(dataList)):
-        moving_average.append(stat.mean(dataList[lower_index:upper_index]))
-        upper_index += 1
-        lower_index += 1
-    return moving_average
+    data = yf.Ticker(stock_symbol) #get the stock data
+    historical_data = data.history(start=start_date, end=end_date) #get the data from start date to end date, to get the indexes
     
+    if historical_data.empty:
+        return None
+
+    starting_row_name = historical_data.iloc[0].name
+    ending_row_name = historical_data.iloc[-1].name
+
+    full_data = data.history(period="max")
+    
+    starting_index = full_data.index.get_loc(starting_row_name) - (n_days - 1) #find our desired position in the max dataframe
+    ending_index = full_data.index.get_loc(ending_row_name) #find our end_date in the max dataframe
+
+    if starting_index < 0: #prevent out of bound errors
+        return None
+    relevant_data = full_data.iloc[starting_index:ending_index + 1]
+    moving_averages = relevant_data['Close'].rolling(window=n_days).mean()
+    aligned_moving_averages = moving_averages.loc[starting_row_name:ending_row_name]
+
+    return aligned_moving_averages
 
 #calculate a n-day exponential moving average
 def nDayEMA(stock_symbol, n_days, start_date, end_date):
@@ -67,15 +65,5 @@ def nDayEMA(stock_symbol, n_days, start_date, end_date):
 def goldenCross(short, long):
     pass
 
-#MACD
-
-
-# Example usage:
-stock_symbol = 'AAPL'  # Apple Inc.
-n_days = 50
-start_date = datetime.datetime(2024, 3, 5)
-end_date = datetime.datetime(2024, 3, 6)
-
-print(calculate_moving_average(stock_symbol, n_days, start_date, end_date))
-thing = [1, 2, 3, 4]
-print(thing[0:len(thing)])
+moving_average = calculate_moving_average('AAPL', 20, '2023-01-01', '2023-03-01')
+print(moving_average)
