b7a2f5687e5cc3e3659a28fad91d815a4a928f8f
RobertRen <robertren123.abc@gmail.com>
2024-03-19 12:54:54 -0700

    EMA in a new file

--- MovingAverages.py
+++ MovingAverages.py
@@ -2,12 +2,10 @@ import math
 import pandas as pd
 import yfinance as yf
 import datetime
-import statistics as stat
     
 def calculate_moving_average(stock_symbol, n_days, start_date, end_date) -> pd.DataFrame:
     data = yf.Ticker(stock_symbol) #get the stock data
     historical_data = data.history(start=start_date, end=end_date) #get the data from start date to end date, to get the indexes
-    
     if historical_data.empty:
         return None
 
@@ -26,37 +24,6 @@ def calculate_moving_average(stock_symbol, n_days, start_date, end_date) -> pd.D
     aligned_moving_averages = moving_averages.loc[starting_row_name:ending_row_name]
 
     return aligned_moving_averages
-
-
-#calculate a n-day exponential moving average
-def nDayEMA(stock_symbol, n_days, start_date, end_date):
-     # Fetch historical market data
-    data = yf.Ticker(stock_symbol)
-    historical_data = data.history(start=start_date, end=end_date)
-    starting_row_name = historical_data.iloc[0].name
-    ending_row_name = historical_data.iloc[len(historical_data) - 1].name
-
-    # Fetch relevant data to find moving average
-    full_data = data.history(period="max")
-    starting_index = full_data.index.get_loc(starting_row_name) - 1
-    ending_index = full_data.index.get_loc(ending_row_name)
-
-    dataList = full_data['Close'].tolist()[starting_index:ending_index]
-    k = 2.0 / (n_days + 1)
-    yesterdays_ema = dataList[0]
-    print(yesterdays_ema)
-    
-    ema = []
-    first = True
-    for price in dataList:
-        if(first):
-            first = False
-            break
-        print(price)
-        new_ema = (price - yesterdays_ema) * k + yesterdays_ema
-        ema.append(new_ema)
-        yesterdays_ema = new_ema
-    return ema
         
 #calculate crosses, and divergence/convergence between two moving averages
 def sgn(n):
@@ -91,4 +58,4 @@ def movingAverageCross(moving_average1: pd.DataFrame, moving_average2: pd.DataFr
 
 moving_average = calculate_moving_average('AAPL', 20, '2023-01-06', '2023-03-04')
 moving_average2 = calculate_moving_average('AAPL', 50, '2023-01-01', '2023-03-04')
-print(movingAverageCross(moving_average, moving_average2))
+print(moving_average)
