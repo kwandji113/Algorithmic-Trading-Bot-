86657641a9ac80442c08ecf9828a11a7df503ffd
RobertRen <robertren123.abc@gmail.com>
2024-03-20 09:18:58 -0700

    changed code to use yf.download() instead to not be pulling the entire ticker data instead

--- EMA.py
+++ EMA.py
@@ -2,22 +2,22 @@ import math
 import pandas as pd
 import yfinance as yf
 import datetime
+from datetime import datetime, timedelta
 
 #calculate a n-day exponential moving average
 def nDayEMA(stock_symbol, n_days, start_date, end_date):
-    data = yf.Ticker(stock_symbol)
+    adjusted_start_date = (datetime.strptime(start_date, '%Y-%m-%d') - timedelta(days=n_days * 1.5)).strftime('%Y-%m-%d') #add a buffer since the stock market isn't open everyday
+    historical_data = yf.download(stock_symbol, adjusted_start_date, end_date)
     
-    historical_data = data.history("max")
     if historical_data.empty:
         return None
 
     # Calculate Exponential Moving Average (EMA)
     ema = historical_data['Close'].ewm(span=n_days, adjust=False).mean()
     
-    return ema
+    aligned_ema = ema.loc[start_date:]
 
-def MACD(stock_symbol):
-    pass
+    return aligned_ema
 
 ema = nDayEMA('AAPL', 20, '2024-01-01', '2024-03-01')
 print(ema)
