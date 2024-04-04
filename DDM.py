5ed61f5319b015734b0f22e5e840de926ea4e3ff
SuenAidan <s-asuen@lwsd.org>
2024-04-03 22:02:51 -0700

    should be done with a basic version of calculating cost of equity using CAPM model

--- DDM.py
+++ DDM.py
@@ -7,7 +7,8 @@ import csv
 
 #guideline source for all calculations: https://web.archive.org/web/20231005030422/https://www.stock-analysis-on.net/NYSE/Market-Risk-Premium
 class DDM: 
-    def __init__(self, ticker_name, start_date):
+    #see if you can delete some of these instance variables and just return instead
+    def __init__(self, ticker_name, start_date: str):
         self.ticker_name = ticker_name
         self.expected_dividend 
         self.cost_of_equity 
@@ -18,14 +19,20 @@ class DDM:
         #flat value for the expected required return because all other ways of calculating seem illogical and this is ultimately up to the user
         #idea for a feature could be asking the user what kind of return they are expecting in the final product 
         self.return_with_risk = .08
+        self.risk_free_rate
 
     #calculates expected dividend per share based on the current dividend payout and growth rate calculated in other methods and sets value to instance variable
     def EEDPS(self):
         pass
 
     #calculates the cost of capital equity using the CAPM model and sets value to instance variable
-    def CCE(self):
-        pass
+    def CCE(self, ticker, starting_date):
+        company = DDM(ticker, starting_date)
+        company.set_beta()
+        company.set_year_and_date()
+        company.set_risk_free_rate()
+        market_risk_premium = self.return_with_risk - self.risk_free_rate
+        self.cost_of_equity = self.risk_free_rate + self.beta * market_risk_premium
 
     #calculates the dividend growth rate using historical data from yFinance and sets value to instance variable
     def DGR(self):
@@ -54,11 +61,24 @@ class DDM:
             # Iterate over each row in the CSV file
             for row in csv_reader:
                 data.append(row)
+        #first item in list is documentation on format of data, need to delete to process and can prob optimize later 
+        data.pop(0)
+
+        #another place to optimize, this is just searching for correct T bill rate based on month and year on linear search, can change to binary or something 
+        for pair in data:
+            #again set year month and day variables based on the delimiter '-' for row
+            year, month, day = pair[0].split('-')
+            #if year and month for current row matches the start date year and month then set risk free limit  
+            if year == self.date[0] and month == self.date[1]:
+                self.risk_free_rate = float(pair[1])
+                return
+            
+
 
     #string processing to override self.date with a tuple containing the year as first element and day as second, just some string processing, assuming that date was passed as the format
     #YEAR-MONTH-DAY
     def set_year_and_date(self):
-        year_index = 
-
-        
+        #split the string into variables of year month and day by delimiter '-'
+        year, month, day = self.date.split('-')
+        self.date = (year, month)
 
