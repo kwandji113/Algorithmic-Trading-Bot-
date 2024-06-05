bc47b602b484243ed29a1a5def1d812b586b694f
SuenAidan <s-asuen@lwsd.org>
2024-06-05 16:49:23 -0700

    code cleanup and more testing

--- testing.py
+++ testing.py
@@ -25,16 +25,7 @@ class Queue(Generic[T]):
         return len(self.queue) == 0
     def length(self) -> int:
         return len(self.queue)
-# data = []
-# # Open the CSV file in read mode
-# with open('TB3MS.csv', 'r') as csv_file:
-#     # Create a CSV reader object
-#     csv_reader = csv.reader(csv_file)
-    
-#     # Iterate over each row in the CSV file
-#     for row in csv_reader:
-#         data.append(row)
-# print(type(data[1]))
+
 
 msft = yf.Ticker("MSFT")
 nvda = yf.Ticker("NVDA")
@@ -52,29 +43,29 @@ print(len(msft.dividends))
 
 dividends = msft.dividends
 
-# # Print the DataFrame
-# print(dividends)
+# Print the DataFrame
+print(dividends)
 
-# # Accessing individual elements in the DataFrame
-# # For example, accessing the first dividend amount and date
-# first_dividend_amount = dividends.iloc[0]
-# print(type(first_dividend_amount))
-# first_dividend_date = dividends.index[0]
-# print(type(first_dividend_date))
+# Accessing individual elements in the DataFrame
+# For example, accessing the first dividend amount and date
+first_dividend_amount = dividends.iloc[0]
+print(type(first_dividend_amount))
+first_dividend_date = dividends.index[0]
+print(type(first_dividend_date))
 
-# print("First Dividend Amount:", first_dividend_amount)
-# print("First Dividend Date:", first_dividend_date)
-# year = first_dividend_date.year
-# print("This was the year", year)
-# print("This is the type of the year " + str(type(year)))
-# month = first_dividend_date.month
-# print("This is the type of the month " + str(type(month)))
-# # info = msft.info
-# # beta = info.get('beta')
-# # print(beta)
-# # ninfo = nvda.info
-# # betan = ninfo.get('beta')
-# # print(type(betan))
+print("First Dividend Amount:", first_dividend_amount)
+print("First Dividend Date:", first_dividend_date)
+year = first_dividend_date.year
+print("This was the year", year)
+print("This is the type of the year " + str(type(year)))
+month = first_dividend_date.month
+print("This is the type of the month " + str(type(month)))
+info = msft.info
+beta = info.get('beta')
+print(beta)
+ninfo = nvda.info
+betan = ninfo.get('beta')
+print(type(betan))
 
 
 import pandas as pd
